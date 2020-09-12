3107482e6ebd3e51a0019a610eab650ef76bef39
uloureiro <urick@urick.com.br>
2020-09-12 18:25:56 -0300

    Add App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import App from './App';
+import axios from 'axios'
+import apiSuccessDataSingle from './api/data/api-success-single.json'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+let renderResult: {
+  container: HTMLElement
+}
+
+describe('Recipe Drawer', () => {
+  const axiosGetSpy = jest.spyOn(axios, 'get')
+  axiosGetSpy.mockResolvedValue(apiSuccessDataSingle)
+  
+  it('renders toolbar', async () => {
+    await act(async () => {
+      renderResult = render(<App />);
+    })
+    const element = renderResult.container.querySelector('#recipe-toolbar');
+    expect(element).toBeInTheDocument();
+  })
+
+  it('renders recipes list', async () => {
+    await act(async () => {
+      renderResult = render(<App />);
+    })
+    const element = renderResult.container.querySelector('#recipe-list');
+    expect(element).toBeInTheDocument();
+  })
+
+  it('recipes list has one recipe', async () => {
+    await act(async () => {
+      renderResult = render(<App />);
+    })
+
+    const element = renderResult.container.querySelector('#asdf1234');
+    expect(element).toBeInTheDocument();
+  })
+})
+
